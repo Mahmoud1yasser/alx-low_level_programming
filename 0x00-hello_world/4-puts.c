e33c5ce562f5b2e4f0de8d44106072780a381aac
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-06-15 13:53:32 -0700

    right answer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-	/* using puts */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
