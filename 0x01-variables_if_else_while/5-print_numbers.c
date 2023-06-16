1db3d708647797ae9811f32b1e66ae9a1681190a
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-06-16 11:40:32 -0700

    add 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: writes sigle decel
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
