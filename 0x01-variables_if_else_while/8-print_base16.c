d6a55620a28b5b6e2055a8b2ef97f4cbdceaafbd
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-06-16 11:58:13 -0700

    add hexadecemal program

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: writes base hexa
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+	int j=97;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 103)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar(10);
+	return (0);
+}
