15823726f0409022c273decc719a0f3b2854e67a
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-06-20 08:31:46 -0700

    upload program 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: print _putchar
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	putchar('_putchar');
+		putchar(10);
+		return (0);
+}
