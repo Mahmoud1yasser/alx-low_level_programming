5494d1c18920ebc36ecddc91b4882344108961eb
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-20 20:53:59 +0300

    add

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+
+/**
+ * main - Entry point
+ * Description: 'print alphabets'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 97;
+while (i < 123)
+	{
+	_putchar(i);
+	i++
+	}
+return (0);
+}
+
+
