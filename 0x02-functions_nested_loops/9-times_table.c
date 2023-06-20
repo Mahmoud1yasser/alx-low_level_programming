8e831552b99479ac034c5f82133b46619d991289
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-21 00:44:36 +0300

    add 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 			if (result <= 9)
 			{
 				_putchar(48 + result);
-				if (r1 != 9)
+				if (num2 != 9)
 				{
 					_putchar(32);
 					_putchar(32);
@@ -36,7 +36,7 @@ void times_table(void)
 			{
 				_putchar(48 + r2);
 				_putchar(48 + r1);
-				if (r1 != 9)
+				if (num2 != 9)
 				{
 					_putchar(32);
 					_putchar(32);
