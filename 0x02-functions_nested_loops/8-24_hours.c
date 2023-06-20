0903e152ea684672e487c416d3ea92ff5c9e4f4c
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-21 00:10:48 +0300

    add 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,34 +13,34 @@ void jack_bauer(void)
 
 	while (min1 <= 2)
 	{
-			min2 = 0;
+		min2 = 0;
 
-			while (min2 <= 9)
+		while (min2 <= 9)
+		{
+			s2 = 0;
+
+			while (s2 <= 5)
 			{
-				s2 = 0;
+				s1 = 0;
 
-				while (s2 <= 5)
+				while (s1 <= 9)
 				{
-					s1 = 0;
-
-					while (s1 <= 9)
-					{
-						_putchar(48 + min1);
-						_putchar(48 + min2);
-						_putchar(58);
-						_putchar(48 + s2);
-						_putchar(48 + s1);
-						_putchar(10);
-						s1++;
-					}
+					_putchar(48 + min1);
+					_putchar(48 + min2);
+					_putchar(58);
+					_putchar(48 + s2);
+					_putchar(48 + s1);
+					_putchar(10);
+					s1++;
+				}
 					s2++;
-				}
-				min2++;
-				if (min1 == 2 && min2 > 3)
-				{
-					break;
-				}
 			}
-		min1++;
+			min2++;
+			if (min1 == 2 && min2 > 3)
+			{
+				break;
+			}
+		}
+	min1++;
 	}
 }
