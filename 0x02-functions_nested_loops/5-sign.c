09e80b87037c5f49477851f41f14ecfde9a3c98f
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-20 21:56:17 +0300

    trial

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * iprint_sign - check sign
+ * Description: 'return sign'
+ * @c: character checked
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar(43);
+	}
+	if else (n == 0)
+	{
+		return (0);
+		_putchar(48);
+	}
+	else
+	{
+		return (-1);
+		_putchar(45);
+	}
+}
+
+
+
