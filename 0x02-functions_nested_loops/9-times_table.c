dada723dc10c10ddbffd73a5139fd89d5aee6840
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-21 00:29:04 +0300

    add 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * Description: 'printer'
+ * @n: number
+ * Return: Always 0 (Success)
+ */
+void times_table(void);
+{
+	int num1;
+	int num2;
+
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+			int result = num1 * num2;
+			int r1 = result % 10;
+			int r2 = result % 100;
+
+			if (result >= 9)
+			{
+				_putchar(48 + result);
+				if (r1 != 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(44);
+				}
+			}
+			else
+			{
+				_putcahr(48 + r2);
+				_putchar(48 + r1);
+				if (r1 != 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(44);
+				}
+			}
+
