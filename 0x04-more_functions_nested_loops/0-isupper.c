ed49d4f274a654a10fc691583ab36fb48fc6c2ab
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-06-22 11:05:48 -0700

    add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - check the code.
+ * Description : ' testing characrters to be lower case'
+ *@c: is character too investgate
+ * Return: Always 0 or 1.
+ */
+int _isupper(int c)
+{
+	if (c <= 90 && c >= 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
