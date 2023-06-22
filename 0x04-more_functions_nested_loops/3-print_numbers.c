922c174c2746f9d0f9a0ded6c48af674368bf60c
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-06-22 11:50:37 -0700

    fix 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,12 @@
 /**
  * print_numbers - print 0 to 9.
  * Description : ' testing character to be digit'
- *@a: is first digit
- *@b: is second digit
- * Return: 0 or 1 (Success)
  */
 void print_numbers(void)
 {
-	int i =48 ;
+	int i = 48;
 
-	while(i <= 57)
+	while (i <= 57)
 	{
 		_putchar(i);
 		i++;
