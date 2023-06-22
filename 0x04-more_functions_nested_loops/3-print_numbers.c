823aa55319db6f154dd5cede9f4cde363a15838a
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-06-22 11:44:27 -0700

    add print numbers file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9.
+ * Description : ' testing character to be digit'
+ *@a: is first digit
+ *@b: is second digit
+ * Return: 0 or 1 (Success)
+ */
+void print_numbers(void)
+{
+	int i =48 ;
+
+	while(i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+}
