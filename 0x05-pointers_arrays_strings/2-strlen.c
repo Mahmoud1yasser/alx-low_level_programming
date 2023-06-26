fb6a5019e4d130e2fd1c18ffa8d7fbb0009e7064
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-06-26 12:03:59 -0700

    fixing condition for empty string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,22 @@
  * _strlen - length of sting
  * Description: 'return data'
  * @s: pointer for data.
+ * Return : count of string characters
  */
 int _strlen(char *s)
 {
 	int i = 1;
+
+	if (*s(0) == 0)
+	{
+		return (0);
+	}
+	else
+	{
 	while (*(s + i) != 0)
 	{
 		i++;
 	}
 	return (i);
+	}
 }
