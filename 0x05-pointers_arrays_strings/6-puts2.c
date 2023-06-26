817cdfb0af9b0828a4f8d5b45bd47075369351bc
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-06-26 14:05:02 -0700

    fixing puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,8 +8,13 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int h = 0;
 
-	while (*(str + i) != 0)
+	while (*(str +h) > 0)
+	{
+		h++;
+	}
+	while ((*(str + i) != 0) && i <= h )
 	{
 		_putchar(*(str + i));
 		i = i + 2;
