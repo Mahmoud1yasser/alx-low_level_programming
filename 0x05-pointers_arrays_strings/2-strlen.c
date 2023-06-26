21e2176ddbcf4603c9a8cb074c76ad3716e67767
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-26 21:51:08 +0300

    adding task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strlen - length of sting
+ * Description: 'return data'
+ * @s: pointer for data.
+ */
+int _strlen(char *s)
+{
+	return (sizeof (*s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 void _putchar(int);
 void reset_to_98(int *);
 void swap_int(int *, int *);
+int _strlen(char *);
