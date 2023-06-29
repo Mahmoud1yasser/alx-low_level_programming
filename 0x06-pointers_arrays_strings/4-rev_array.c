471dd8e4e1f648f5bc5d25cc12416a62eae80e87
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-29 14:29:42 +0300

    fixing bugs task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,22 +10,10 @@ void reverse_array(int *a, int n)
 	int j;
 	int temp;
 
-	if (n % 2 == 0)
-	{
 		for (j = 0; j < n / 2; j++)
 		{
 			temp = a[j];
 			a[j] = a[n - j - 1];
 			a[n - j - 1] = temp;
 		}
-	}
-	else
-	{
-		for (j = 0; j < ((n / 2) - 1); j++)
-		{
-			temp = a[j];
-			a[j] = a[n - j - 1];
-			a[n - j - 1] = temp;
-		}
-	}
 }
