54216215d117b02bfceac413d33e7cd83260fb6e
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-29 17:38:50 +0300

    fixing bugs

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,21 @@
 char *cap_string(char *str)
 {
 	int i;
+	int j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33,
+		40, 41, 63, 123, 125, 34, 2};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (
-				i == 0 ||str[i - 1] == ' ' || str[i - 1] == '\t' ||
-				str[i - 1] == '\n' || str[i - 1] == ',' ||
-				str[i - 1] == ';' || str[i - 1] == '.' ||
-				str[i - 1] == '!' || str[i - 1] == '?' ||
-				str[i - 1] == '"' || str[i - 1] == '(' ||
-				str[i - 1] == ')' || str[i - 1] == '{' ||
-				str[i - 1] == '}'
-				)
+		for (j = 0; j < 14; j++)
 		{
-				if (str[i] > 96 && str[i] < 123)
+			if (str[i] == a[j])
+			{
+				if (str[i + 1] > 96 && str[i + 1] < 123)
 				{
-					str[i] -= 32;
+					str[i + 1] -= 32;
 				}
+			}
 		}
 	}
 	return (str);
