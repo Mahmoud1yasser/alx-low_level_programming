d692503c5c6649091c8bd39b5eadfb911038b12f
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-29 10:45:03 +0300

    add task 2 solution

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strncpy - copy
+ * description: copy string to another
+ * with n bytes.
+ * @dest: destenation string
+ * @src: string to be added
+ * @n: number of bytes
+ * Return : pointer to sting dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+	int i = 0;
+
+	for (j = 0; (j < n) && (*(src + i) != '\0'); j++)
+	{
+		*(dest + j) = *(src + i);
+		i++;
+	}
+	for ( ; j < n; j++)
+	{
+		*(dest +j) = '\0';
+	}
+	return (dest);
+}
