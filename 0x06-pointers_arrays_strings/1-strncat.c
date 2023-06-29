4a63825e92147956dea08d825bbb61a9bb21dcd0
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-29 10:29:15 +0300

    adding task 1 solution

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * *_strcat - concatenation
+ * *_strncat - concatenation
  * description: concatenate two strings
+ * with n bytes
  * @dest: destenation string
  * @src: string to be added
+ * @n: number of bytes
  * Return : pointer to sting dest.
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
 	int j;
+	int i = 0;
 	int len;
 
 	for (len = 0; *(dest + len) != '\0'; len++)
 	{}
-	for (j = 0; *(src + i) != '\0'; j++)
+	for (j = 0; (j < n) && (*(src + i) != '\0'); j++)
 	{
 		*(dest + len + j) = *(src + i);
 		i++;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 void _putchar(int);
 char *_strcat(char *, char *);
+char *_strncat(char *, char *, int );
