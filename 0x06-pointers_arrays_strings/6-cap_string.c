009a9b392c982d9c5db1e72cfc6ebb0b7a2199f1
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-29 17:29:32 +0300

    new betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,13 +12,15 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+		if (
+				i == 0 ||str[i - 1] == ' ' || str[i - 1] == '\t' ||
 				str[i - 1] == '\n' || str[i - 1] == ',' ||
 				str[i - 1] == ';' || str[i - 1] == '.' ||
 				str[i - 1] == '!' || str[i - 1] == '?' ||
 				str[i - 1] == '"' || str[i - 1] == '(' ||
 				str[i - 1] == ')' || str[i - 1] == '{' ||
-				str[i - 1] == '}')
+				str[i - 1] == '}'
+				)
 		{
 				if (str[i] > 96 && str[i] < 123)
 				{
