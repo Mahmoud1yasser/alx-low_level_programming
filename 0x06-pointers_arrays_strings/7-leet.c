5e6ac10a3ad2ff1ff8f488ab59ddf9708b43e25f
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-29 17:00:18 +0300

    revise betty

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,7 +16,7 @@ char *leet(char *str)
 
 	for (j = 0; str[j] != '\0'; j++)
 	{
-		for (i = 0; i < 4; i++)
+		for (i = 0; i <= 4; i++)
 			if (str[j] == lower[i] || str[j] == upper[i])
 			{
 				str[j] = i1337[i];
