ebf6cc9eadd96dc95f98091805f52e1ce14c191c
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-29 14:09:15 +0300

    fixig task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,33 +4,14 @@
  * description: compare two strings.
  * @s1: 1st string
  * @s2: 2nd string
- * Return : value.
+ * Return: value of diffrent string.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1;
-	int len2;
-	int total1 = 0;
-	int total2 = 0;
-
-	for (len1 = 0; *(s1 +len1) != '\0'; len1++)
+	while (*s1 && (*s1 == *s2))
 	{
-		total1 = *(s1 + len1) + total1;
+		s1++;
+		s2++;
 	}
-	for (len2 = 0; *(s2 +len2) != '\0'; len2++)
-	{
-		total2 = *(s2 + len2) + total2;
-	}
-	if (total1 == total2)
-	{
-		return (0);
-	}
-	else if (total1 > total2)
-	{
-		return (total1 - total2);
-	}
-	else
-	{
-		return (total1 - total2);
-	}
+	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
