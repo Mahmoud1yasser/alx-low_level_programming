567e58be835c19929d6aea7d27cb528e3ac39537
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-06-29 19:16:03 +0300

    advancd 1

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,11 +12,11 @@ char *rot13(char *str)
 	int i;
 	char dec[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char enc[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	char *result = malloc(strlen(str) + 1);
+
 	for (j = 0; str[j] != '\0'; j++)
 	{
 		for (i = 0; i < 52; i++)
-		{	
+		{
 			if (*(str + j) == *(dec + i))
 			{
 				*(str + j) = *(enc + i);
