809aac3dff13478124978e4942d6949ecb4c4e79
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-03 12:45:54 +0300

    trail

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#define NULL ((void *)0)
 /**
  * _strchr - search character
  * Description: 'search for indecated
@@ -22,7 +23,7 @@ char *_strchr(char *s, char c)
 	}
 	if (!i && !found)
 	{
-		s = '\0';
+		return (NULL);
 	}
 	return (s);
 }
