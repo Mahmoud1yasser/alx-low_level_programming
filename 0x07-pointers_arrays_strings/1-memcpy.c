fc051b546a3f1edad62371411c3a2cbf648de158
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-03 10:49:41 +0300

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memcpy - copy memory
+ * Description: 'copy memory from
+ * src to dest'
+ * @dest: destenation pointer.
+ * @src: data pointer .
+ * @n: amount of bytes
+ *
+ * Return: Always dest (Success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char temp;
+
+	for(i = 0; i < n; i++)
+	{
+		temp = *(src + i);
+		*(dest + i) = temp;
+	}
+return (dest);
+}
