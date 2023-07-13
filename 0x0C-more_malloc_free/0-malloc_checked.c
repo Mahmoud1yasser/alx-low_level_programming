39f5d80fea41ef00024ffb816b40c70a38edde75
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-13 21:16:37 +0300

    add task 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - checkes for memory
+ * allocation.
+ * @b: size of memory.
+ * Return: 98 (Success).
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *n = (int*)malloc(b);
+	if(n == NULL)
+	{
+	exit(98);
+	}
+	return (n);
+}
