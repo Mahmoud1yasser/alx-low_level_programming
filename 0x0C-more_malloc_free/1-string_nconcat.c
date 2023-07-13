131f4fb4c180722da1229ac79f49d8d889cc7f1f
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-13 22:48:07 +0300

    fix task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,8 +23,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
 	if (n >= len2)
 	{
 		n = len2;
