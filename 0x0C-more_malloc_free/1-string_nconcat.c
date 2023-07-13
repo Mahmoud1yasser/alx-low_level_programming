88f23f486ea3c7a4cac863c54612ebd1af2cda9f
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-14 00:35:29 +0300

    trial

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,10 +22,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+	for (len1 = 0; s1[len1] != '\0'; len1++);
+	for (len2 = 0; s2[len2] != '\0'; len2++);
 	if (n >= len2)
 	{
 		n = len2;
@@ -37,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < len1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		conc[i] = s1[i];
 	}
@@ -45,6 +43,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		conc[len1 + i] = s2[i];
 	}
-	conc[len1 + n] = '\0';
+	conc[len1 + n + 1] = '\0';
 	return (conc);
 }
