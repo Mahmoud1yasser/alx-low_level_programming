83cadb87c4ad2a3aedbc47414320f33dd74f5940
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-13 23:16:21 +0300

    fix 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	return (NULL);
 	}
 	tmp = prt;
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0;i < nmemb * size; i++)
 	{
 			*tmp++ = 0;
 	}
