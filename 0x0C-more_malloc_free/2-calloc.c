d49d30429321c48d06d06c0e26d2850e97b6d7a5
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-13 22:28:27 +0300

    add task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @nmemb: array of elements.
+ * @size: size of array
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *prt = malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	if (prt == NULL)
+	{
+	return (NULL);
+	}
+	return (prt);
+}
