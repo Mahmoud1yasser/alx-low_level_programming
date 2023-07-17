38f09c2e4f917c89f6bb395c7ce67be72c09fb0f
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-17 18:30:07 +0300

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - A dog is the only thing 
+ * on earth that loves you more than
+ * you love yourself.
+ * @d: data structure.
+ * @name: dog name.
+ * age: dog age.
+ * owner: dog owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
