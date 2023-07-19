deee17c2341808caad9477e8d1fff1628ff9cad2
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-19 20:11:12 +0300

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <string.h>
 /*
  * print_name - prints a name as is
  * @name: name of the person
@@ -8,10 +7,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
 	f(name);
-
 }
