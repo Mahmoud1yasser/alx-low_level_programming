eeba50895e8c87db7c2276a49f770ee6599ac59a
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-19 23:25:58 +0300

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,27 +6,27 @@
  * @argv:equation string
  * Return: 0 (Success)
  */
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[3]);
-	int(*operator)(int,int);
+	int (*operator)(int, int);
 
 	operator = get_op_func(argv[2]);
 	if ((argv[2][0] == '%' || argv[2][0] == '/') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
-		return(100);
+		return (100);
 	}
 	if (operator == NULL)
 	{
 		printf("Error\n");
-		return(99);
+		return (99);
 	}
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return 98;
+		return (98);
 	}
 	printf("%d\n", operator(num1, num2));
 	return (0);
