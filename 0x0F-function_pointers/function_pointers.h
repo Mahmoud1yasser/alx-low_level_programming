edb7fcb4a6f1806342971058c2030ca8f9937f88
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-19 20:27:17 +0300

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
 #include <stdlib.h>
+
 void print_name(char *, void (*f)(char *));
 #endif
