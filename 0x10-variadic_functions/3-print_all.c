1fef41530df9d3649506b1ff78a074b58274e937
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-20 16:14:24 +0300

    trial

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,79 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
- * print_all - prints all types,
- * followed by a new line
- * @format: list of types of arguments
+ * print_int - prints an int
+ * @args: the list of args
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+/**
+ * print_char - prints a char
+ * @args: the list of args
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+/**
+ * print_string - prints a string
+ * @args: the list of args
+ */
+void print_string(va_list args)
+{
+	char *z = va_arg(args, char *);
+
+	if (!z)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", z);
+}
+/**
+ * print_float - prints floats
+ * @args: the list of args
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+/**
+ * print_all - prints all
+ * @format: formats of arg
  */
 void print_all(const char * const format, ...)
 {
-	va_list ap;
-	const char *p = format;
+	types_t types[] = {
+	{'c', print_char},
+	{'i', print_int},
+	{'f', print_float},
+	{'s', print_string},
+	{'\0', NULL}
+	};
+	va_list args;
+	char *sep1 = "", *sep2 = ", ";
+	int count1 = 0, count2 = 0;
 
-	va_start(ap, format);
-	while (*p != '\0')
+	va_start(args, format);
+	while (format !=  NULL && format[count1] != '\0')
 	{
-		switch (*p)
+		count2 = 0;
+		while (types[count2].z != '\0')
 		{
-			case 'i':
-				printf("%d ", va_arg(ap, int));
-				break;
-			case 'f':
-				printf("%f ", va_arg(ap, double));
-				break;
-			case 's':
-				{
-					char *temp = va_arg(ap, char *);
-					if (temp != NULL)
-					{
-						printf("%s ", temp);
-					}
-					else
-					{
-						printf("%s ", "(nil)");
-					}
-					break;
-				}
-			case 'c':
-				printf("%c ", va_arg(ap, int));
-				break;
-			default:
-				break;
+			if (format[count1] == types[count2].z)
+			{
+				printf("%s", sep1);
+				types[count2].f(args);
+				sep1 = sep2;
+			}
+			count2++;
 		}
-		p++;
+		count1++;
 	}
-	va_end(ap);
-	putchar('\n');
+	printf("\n");
+	va_end(args);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,25 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+int _putchar(char c);
+/**
+ * struct pter - pointer to print all
+ * @z: pointer to input for test
+ * @f: pointer to function
+ */
+typedef struct types
+{
+	char z;
+	void (*f)(va_list);
+} types_t;
 
+void print_int(va_list);
+
+void print_char(va_list);
+
+void print_float(va_list);
+
+void print_string(va_list);
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int, ...);
