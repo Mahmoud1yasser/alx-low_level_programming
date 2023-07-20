b7c3e7cf32e96645987133f167cb8478d938a55d
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-20 12:33:50 +0300

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums integrs
+ * @n: integers list
+ * Return: result
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n != 0)
+	{
+		va_list ap;
+		int i;
+		unsigned int j = n;
+		int result = 0;
+
+		va_start(ap, n);
+		i= va_arg(ap, int);
+		for (; j > 0; i = va_arg(ap, int))
+		{
+			result = i + result;
+			j--;
+		}
+		return (result);
+		va_end(ap);
+	}
+	return (0);
+}
