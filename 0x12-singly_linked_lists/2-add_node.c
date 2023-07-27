46ab5f0f0604a3c4565504358dcabf098a083ddd
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-27 22:08:26 +0300

    add task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,19 +5,17 @@
  * add_node - function returns the number
  * of elements in a linked
  * of data.
- * @head: pointer to pointer head
+ * @*head: pointer to pointer head
  * @str: string to be added
  * Return: pointer to added node
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *linkedlist = *head;
-	list_t *lk = (list_t*) malloc(sizeof(list_t));
+	list_t *lk = malloc(sizeof(list_t));
 	if (lk == NULL)
 		return (NULL);
 	lk->str = strdup(str);
-	while(linkedlist->next != NULL)
-		linkedlist = linkedlist->next;
-	linkedlist->next = lk;
-	return (linkedlist->next);
+	lk->next = *head;
+	*head = lk;
+	return (lk);
 }
