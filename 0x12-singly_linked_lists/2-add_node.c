f6409707d9725d008bb1a1a3caafe44e6ffef16a
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-27 22:17:09 +0300

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,13 +5,14 @@
  * add_node - function returns the number
  * of elements in a linked
  * of data.
- * @*head: pointer to pointer head
+ * @head: pointer to pointer head
  * @str: string to be added
  * Return: pointer to added node
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *lk = malloc(sizeof(list_t));
+
 	if (lk == NULL)
 		return (NULL);
 	lk->str = strdup(str);
