9cd9fffb871c29f13ca335108bc66c41ec8f5b7e
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-31 21:21:07 +0300

    adding task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function to free data in nodes
+ * @head: first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+	listint_t *p = *head;
+
+	while (p != NULL)
+	{
+		current = p;
+		p = p->next;
+		free(current);
+	}
+	*head = NULL;
+}
