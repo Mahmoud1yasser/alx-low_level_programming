190e04c9b1df1b3c05006c422f27184d7586b565
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-31 23:20:04 +0300

    add task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,6 +41,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			find->next = new;
 			return (new);
 		}
+		if (idx > pos)
+			return(NULL);
 	}
 	return (NULL);
 }
