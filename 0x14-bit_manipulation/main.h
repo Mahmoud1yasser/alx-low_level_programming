e16cc84a740090cf50f1cf5c2cf7459d4c06633e
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-08-03 21:18:49 +0300

    add task 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_int(unsigned int num, unsigned int len);
+#endif
