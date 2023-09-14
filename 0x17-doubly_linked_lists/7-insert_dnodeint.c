b6ad03d78d5b79e09f16fcaefdaebd196a4ec2d7
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-09-14 13:35:28 -0700

    update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,65 +1,39 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - function to insert node at given index
- * @h: double pointer type to head of list
- * @idx: index at which node will be added
- * @n: int type for data to be added
- * Return: node if successful, NULL if failed
+ * insert_dnodeint_at_index - Insert a node in the postion
+ * @h: Pointer to direction of the head
+ * @n: The data integer
+ * @idx: Position at the insert the new node
+ * Return: The direction of the new node
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *node = malloc(sizeof(dlistint_t));
-	dlistint_t *temp;
-	unsigned int count = 0;
+	dlistint_t *new_mem, *res_mem = *h;
+	/* unsigned int counter = 0 */
 
-	if (node == NULL)
-		return (NULL);
-	node->n = n;
-	node->prev = NULL;
-	node->next = NULL;
-
-	if (*h == NULL)
-	{
-		*h = node;
-		node->next = NULL;
-		return (node);
-	}
-	temp = *h;
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		node->next = temp;
-		temp->prev = node;
-		*h = node;
-		return (node);
-	}
-	cont(void);
-	return (node);
-}
-/**
- * cont - check the code
- */
-void cont(void)
-{
-	while (count != (idx - 1))
-	{
-		temp = temp->next;
-		count++;
-		if (temp == NULL)
-		{
-			free(node);
+		if (res_mem == NULL)
 			return (NULL);
-		}
+		res_mem = res_mem->next;
 	}
-	node->next = temp->next;
-	node->prev = temp;
-	if (temp->next == NULL)
-	{
-		temp->next = node;
-	}
-	else
-	{
-		temp->next->prev = node;
-		temp->next = node;
-	}
+
+	if (res_mem->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_mem = malloc(sizeof(dlistint_t));
+
+	if (new_mem == NULL)
+		return (NULL);
+
+	new_mem->n = n;
+	new_mem->next = res_mem->next;
+	new_mem->prev = res_mem;
+	res_mem->next->prev = new_mem;
+	res_mem->next = new_mem;
+
+	return (new_mem);
 }
