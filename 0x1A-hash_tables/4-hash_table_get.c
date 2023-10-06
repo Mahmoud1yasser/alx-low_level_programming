fd04611dbe4c50bfc682acbd7b043fed70af8302
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-10-06 11:47:51 -0700

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -23,13 +23,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	while (index < ht->size)
 	{
-		while(temp != NULL)
+		while (temp != NULL)
 		{
 			if (strcmp(temp->key, key) == 0)
-				return temp->value;
+				return (temp->value);
 			temp = temp->next;
 		}
-		index ++;
+		index++;
 	}
 	return (NULL);
 }
