0dd60b1d153ceb01133d7f917a03fa26d65f5b5d
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-10-06 11:17:33 -0700

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	if (strcmp("", key) == 0)
-		return (0)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new = malloc(sizeof(hash_node_t));
                 if (new == NULL)
