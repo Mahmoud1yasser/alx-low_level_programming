caae4b0e3ae5ceeeca5af27b0a4db04e14ca562f
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-10-06 11:45:14 -0700

    update

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - function to retrieve value at key
+ * @ht: is the hash table
+ * @key: is the key being searched
+ * Return: associated value if found or NULL if not
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int index;
+
+	if (!ht || !ht->array || !key || strlen(key) == 0)
+	{
+		return (NULL);
+	}
+	index = key_index((const unsigned char *) key, ht->size);
+	temp = ht->array[index];
+	if (ht->array[index] == NULL)
+		return (NULL);
+	while (index < ht->size)
+	{
+		while(temp != NULL)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return temp->value;
+			temp = temp->next;
+		}
+		index ++;
+	}
+	return (NULL);
+}
